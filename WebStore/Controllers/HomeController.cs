8bc4c87c0ac4e539e35c6b5e8f95242e57b2e91c
SobbikashiGit <xt207@yandex.ru>
2021-09-28 12:34:24 +0300

    Сотрудники магазина

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+
+        };
         private readonly IConfiguration _Configuration;
 
         public HomeController(IConfiguration Configuration)
@@ -15,9 +24,13 @@ namespace WebStore.Controllers
         {
             return View();
         }
-        //public IActionResult SecondAction()
-        //{
-        //    return View();
-        //}
+        public IActionResult SecondAction()
+        {
+            return View("Index");
+        }
+        public IActionResult Employees()
+        {
+            return View(__Employees);
+        }
     }
 }
