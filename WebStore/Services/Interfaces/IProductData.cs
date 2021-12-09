caf094376d68ec09c2a78d95360532188b971ad8
SobbikashiGit <xt207@yandex.ru>
2021-12-09 12:29:56 +0300

    Определение интерфейса сервиса каталога товаров

---
+++ WebStore/Services/Interfaces/IProductData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Services.Interfaces
+{
+    interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+        IEnumerable<Brand> GetBrands();
+    }
+}
