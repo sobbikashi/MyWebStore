5a5a655304dbf636de64936a759b7193ade4e682
SobbikashiGit <xt207@yandex.ru>
2021-12-09 12:16:45 +0300

    Перенос сотрудников в отдельный класс тестовых данных Базовые классы и интерфейсы сущностей

--- WebStore/Services/InMemoryEmployeesData.cs
+++ WebStore/Services/InMemoryEmployeesData.cs
@@ -3,34 +3,38 @@ using WebStore.Services.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using WebStore.Data;
 
 namespace WebStore.Services
 {
     public class InMemoryEmployeesData : IEmployeesData
-    {
-
-        private readonly List<Employee> _Employees = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
-            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
-
-        };
+    {                
         private int _CurrentMaxId;
 
-        public InMemoryEmployeesData() => _CurrentMaxId = _Employees.Max(i => i.Id);
-        public IEnumerable<Employee> GetAll() => _Employees;
+        public InMemoryEmployeesData()
+        {
+            _CurrentMaxId = TestData.Employees.Max(i => i.Id);
+        }
 
-        public Employee Get(int id) => _Employees.SingleOrDefault(employee => employee.Id == id);        
+        public IEnumerable<Employee> GetAll()
+        {
+            return TestData.Employees;
+        }
+
+        public Employee Get(int id)
+        {
+            return TestData.Employees.SingleOrDefault(employee => employee.Id == id);
+        }
+
         public int Add(Employee employee)
         {
             if (employee is null) throw new ArgumentNullException(nameof(employee));
 
-            if (_Employees.Contains(employee)) return employee.Id; //только для inMemory, для БД удалить, не учитывается наличие полных тёзок одного возраста
+            if (TestData.Employees.Contains(employee)) return employee.Id; //только для inMemory, для БД удалить, не учитывается наличие полных тёзок одного возраста
 
             employee.Id = ++_CurrentMaxId;
 
-            _Employees.Add(employee);
+            TestData.Employees.Add(employee);
 
             return employee.Id;
         }
@@ -39,7 +43,7 @@ namespace WebStore.Services
         {
             if (employee is null) throw new ArgumentNullException(nameof(employee));
 
-            if (_Employees.Contains(employee)) return; //тоже только для inMemory
+            if (TestData.Employees.Contains(employee)) return; //тоже только для inMemory
 
             var db_item = Get(employee.Id);
 
@@ -62,7 +66,7 @@ namespace WebStore.Services
 
             if (db_item is null) return false;
 
-            return _Employees.Remove(db_item);
+            return TestData.Employees.Remove(db_item);
         }     
  
     }
