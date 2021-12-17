c9890a9c7339ec887f67c8c7b3aa003ef121446f
SobbikashiGit <xt207@yandex.ru>
2021-12-17 13:31:13 +0300

    Визуальный компонент брендов

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+using System.Linq;
+using WebStore.ViewModels;
+using System.Collections.Generic;
+
+namespace WebStore.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductData _ProductData;
+
+        public BrandsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+        public IViewComponentResult Invoke() => View(GetBrands());
+       public IEnumerable<BrandsViewModel> GetBrands() =>
+            _ProductData.GetBrands()
+            .OrderBy(b => b.Order).Select(b => new BrandsViewModel
+            {
+                Id = b.Id,
+                Name = b.Name,
+            });
+
+
+    }
+}
---
+++ WebStore/ViewModels/BrandsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.ViewModels;
+
+namespace WebStore.ViewModels
+{
+    public class BrandsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
