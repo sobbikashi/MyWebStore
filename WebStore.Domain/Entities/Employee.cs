a70101458508b2da0a00f4cc954a58b140b595a2
SobbikashiGit <xt207@yandex.ru>
2022-03-09 15:48:17 +0300

    Очередной контекст ДБ

--- WebStore.Domain/Entities/Employee.cs
+++ WebStore.Domain/Entities/Employee.cs
@@ -9,11 +9,8 @@ namespace WebStore.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string FirstName { get; set; }
-        [Required]
         public string LastName { get; set; }
         public string Patronymic { get; set; }
-
-        [Column(TypeName ="decimal(18,2)")]
         public int Age { get; set; }
     }
     //public record Employee2(int Id, string FirstName, string LastName, string Patronymic, int Age);
