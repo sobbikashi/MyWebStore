d465fa8c0580966e87aac362bfa0c370cda241ee
SobbikashiGit <xt207@yandex.ru>
2022-03-11 16:01:18 +0300

    Доменные модели пользователя и роли

---
+++ WebStore.Domain/Entities/Identity/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Domain.Entities.Identity
+{
+    public class User : IdentityUser
+    {
+        public const string Administrator = "Admin";
+        public const string DefaultAdminPassword = "AdPass_123";
+    }
+
+    public class Role : IdentityRole
+    {
+        public const string Administrators = "Administrators";
+        public const string Users = "Users";
+    }
+}
