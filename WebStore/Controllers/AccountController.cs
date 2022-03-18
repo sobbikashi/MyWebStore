4500732c29c067d56e0ae47f9b0fc18beeb9a5d5
SobbikashiGit <xt207@yandex.ru>
2022-03-18 14:02:36 +0300

    Логика входа пользователя в систему

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -16,6 +16,8 @@ namespace WebStore.Controllers
             _UserManager = UserManager;
             _SignInManager = SignInManager;
         }
+
+        #region Register
         public IActionResult Register() => View(new RegisterUserViewModel());
         [HttpPost, ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterUserViewModel Model)
@@ -35,7 +37,41 @@ namespace WebStore.Controllers
                ModelState.AddModelError("", error.Description);
             return View(Model);
         }
-        public IActionResult Login() => View();
+        #endregion
+
+        public IActionResult Login(string ReturnUrl) => View(new LoginViewModel {ReturnUrl = ReturnUrl });
+        
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel Model)
+        {
+            if (!ModelState.IsValid) return View(Model);
+            var loginn_result = await _SignInManager.PasswordSignInAsync(
+                Model.UserName, 
+                Model.Password, 
+                Model.RememberMe,
+#if DEBUG
+                false
+#else
+                true
+#endif
+
+                );
+            if (loginn_result.Succeeded)
+            {
+                //return Redirect(Model.ReturnUrl); //НЕЛЬЗЯ!! НЕ БЕЗОПАСНО!!!!
+                #region
+                //if (Url.IsLocalUrl(Model.ReturnUrl))
+                //    return Redirect(Model.ReturnUrl);
+                //else
+                //    return RedirectToAction("Index", "Home");
+                #endregion
+                return LocalRedirect(Model.ReturnUrl); //аналог вышенаписанного региона, но одной строчкой
+            }
+            ModelState.AddModelError("", "Ошибка в имени пользователя или в пароле");
+            return View(Model);
+
+        }
         public IActionResult Logout() => RedirectToAction("Index", "Home");
         public IActionResult AccessDenied() => View();
     }
---
+++ WebStore/ViewModels/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name ="Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить меня")]
+        public bool RememberMe { get; set; }
+
+        [HiddenInput(DisplayValue =false]
+        public string ReturnUrl { get; set; }
+    }
+}
